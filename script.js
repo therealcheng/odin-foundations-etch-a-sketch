bb238266eca8cd6257d4db0d14ec422d4ee22c59
Cheng Lim <wenyong.lim@icloud.com>
2022-09-17 18:53:00 +0100

    Better code for grid function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict';
 const gridContainer = document.querySelector('.grid-container');
-console.log(gridContainer);
 
-function makeRows(rows, cols) {
+function makeGrid(rows, cols) {
   gridContainer.style.setProperty('--grid-rows', rows);
   gridContainer.style.setProperty('--grid-cols', cols);
-  for (let c = 0; c < rows * cols; c++) {
-    let cell = document.createElement('div');
-    cell.innerText = c + 1;
-    gridContainer.appendChild(cell).className = 'grid-item';
+  const totalCells = rows * cols;
+  for (let cell = 0; cell < totalCells; cell++) {
+    let emptyCell = document.createElement('div');
+    emptyCell.innerText = cell + 1;
+    gridContainer.appendChild(emptyCell).className = 'grid-item';
   }
 }
 
-makeRows(6, 6);
+makeGrid(7, 7);
