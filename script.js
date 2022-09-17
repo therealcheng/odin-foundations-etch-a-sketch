8bba44452d45d1d40afd78c31a39684acddb34b8
Cheng Lim <wenyong.lim@icloud.com>
2022-09-17 18:48:43 +0100

    Created Grids

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 'use strict';
-const grid = document.querySelector('.box-grid');
-console.log(grid);
+const gridContainer = document.querySelector('.grid-container');
+console.log(gridContainer);
+
+function makeRows(rows, cols) {
+  gridContainer.style.setProperty('--grid-rows', rows);
+  gridContainer.style.setProperty('--grid-cols', cols);
+  for (let c = 0; c < rows * cols; c++) {
+    let cell = document.createElement('div');
+    cell.innerText = c + 1;
+    gridContainer.appendChild(cell).className = 'grid-item';
+  }
+}
+
+makeRows(6, 6);
